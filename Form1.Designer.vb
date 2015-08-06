75986559a867a6477468cc4165261cc2cf25c73a
bochi <bokilin@gmail.com>
2015-08-06 15:35:20 +0800

    eee

---
+++ Form1.Designer.vb
@@ -0,0 +1,198 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblStartTime = New System.Windows.Forms.Label()
+        Me.lblEndTime = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblRessult = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblWestTime = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(303, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(139, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "GetCurrentVer"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(448, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(141, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "GetActivityDataListAll"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 12)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "StartTime"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(155, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(48, 12)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "EndTime"
+        '
+        'lblStartTime
+        '
+        Me.lblStartTime.AutoSize = True
+        Me.lblStartTime.Location = New System.Drawing.Point(68, 9)
+        Me.lblStartTime.Name = "lblStartTime"
+        Me.lblStartTime.Size = New System.Drawing.Size(47, 12)
+        Me.lblStartTime.TabIndex = 4
+        Me.lblStartTime.Text = "00:00:00"
+        '
+        'lblEndTime
+        '
+        Me.lblEndTime.AutoSize = True
+        Me.lblEndTime.Location = New System.Drawing.Point(209, 9)
+        Me.lblEndTime.Name = "lblEndTime"
+        Me.lblEndTime.Size = New System.Drawing.Size(47, 12)
+        Me.lblEndTime.TabIndex = 5
+        Me.lblEndTime.Text = "00:00:00"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(24, 30)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 12)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Ressult"
+        '
+        'lblRessult
+        '
+        Me.lblRessult.AutoSize = True
+        Me.lblRessult.Location = New System.Drawing.Point(68, 30)
+        Me.lblRessult.Name = "lblRessult"
+        Me.lblRessult.Size = New System.Drawing.Size(0, 12)
+        Me.lblRessult.TabIndex = 7
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 60)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(555, 319)
+        Me.DataGridView1.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(175, 30)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 12)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "花費時間"
+        '
+        'lblWestTime
+        '
+        Me.lblWestTime.AutoSize = True
+        Me.lblWestTime.Location = New System.Drawing.Point(234, 30)
+        Me.lblWestTime.Name = "lblWestTime"
+        Me.lblWestTime.Size = New System.Drawing.Size(11, 12)
+        Me.lblWestTime.TabIndex = 10
+        Me.lblWestTime.Text = "0"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(295, 30)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(18, 12)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "ms"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(448, 33)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(141, 23)
+        Me.Button3.TabIndex = 12
+        Me.Button3.Text = "GetActivityDataListAll"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(744, 407)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.lblWestTime)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.lblRessult)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblEndTime)
+        Me.Controls.Add(Me.lblStartTime)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblStartTime As System.Windows.Forms.Label
+    Friend WithEvents lblEndTime As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents lblRessult As System.Windows.Forms.Label
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents lblWestTime As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,62 @@
+﻿
+Public Class Form1
+    Dim ws As New WebReference.InfoCenterWebService
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Try
+            Dim Watch As New Stopwatch
+            'Dim iStart As Integer = 0
+            'Dim iEnd As Integer = 0
+            Watch.Start()
+            lblStartTime.Text = Now.ToString("HH:mm:ss:ffff")
+            'iStart = Now.ToString("ffff")
+            lblRessult.Text = ws.GetCurrentVer()
+            lblEndTime.Text = Now.ToString("HH:mm:ss:ffff")
+            'iEnd = Now.ToString("ffff")
+            Watch.Stop()
+            lblWestTime.Text = Watch.ElapsedMilliseconds
+
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Dim Watch As New Stopwatch
+            'Dim iStart As Integer = 0
+            'Dim iEnd As Integer = 0
+            Dim ds As New DataSet
+            Watch.Start()
+            lblStartTime.Text = Now.ToString("HH:mm:ss:ffff")
+            'iStart = Now.ToString("ffff")
+            ds = ws.GetActivityDataListAll
+            DataGridView1.DataSource = ds.Tables(0)
+            lblEndTime.Text = Now.ToString("HH:mm:ss:ffff")
+            'iEnd = Now.ToString("ffff")
+            Watch.Stop()
+            lblWestTime.Text = Watch.ElapsedMilliseconds
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            Dim o As New wsService.UserWS
+            Dim s As String
+            s = o.getUserInfo("123123", "sss")
+            MsgBox(s)
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+End Class
